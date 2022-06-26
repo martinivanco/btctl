1c65964d53847213e58e9a740f4b03bca38b38ee
Martin Ivanco <ivancom.fr@gmail.com>
2022-06-26 15:17:44 +0200

    Connecting devices

--- btctl/BluetoothControl.swift
+++ btctl/BluetoothControl.swift
@@ -6,13 +6,17 @@
 //
 
 import ArgumentParser
+import Foundation
 
 struct BluetoothControl: ParsableCommand {
     static let configuration = CommandConfiguration(
-        abstract: "Bluetooth control utility.",
-        subcommands: [Connected.self, Paired.self])
+        commandName: "btctl",
+        abstract: "Bluetooth control utility",
+        subcommands: [Connected.self, Paired.self, Connect.self])
     
     struct Connected: ParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Get list of connected devices")
+
         func run() {
             guard let devices = BluetoothFramework.connectedDevices else {
                 return print("Error: Couldn't fetch connected devices!")
@@ -23,12 +27,32 @@ struct BluetoothControl: ParsableCommand {
     }
     
     struct Paired: ParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Get list of paired devices")
+
         func run() {
             guard let devices = BluetoothFramework.pairedDevices else {
                 return print("Error: Couldn't fetch paired devices!")
             }
 
             for d in devices { print("\(d.address)\t\(d.name)") }
+        }
+    }
+    
+    struct Connect: ParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Connect to a bluetooth device")
+
+        @Argument(help: "The address of the device to connect")
+        var address: String
+        
+        @Option(name: .shortAndLong, help: "Optional timeout for the call in seconds")
+        var timeout: TimeInterval?
+
+        func run() {
+            if BluetoothFramework.connect(address: address, timeout: timeout) {
+                print("Successfully connected \(address).")
+            } else {
+                print("Error: Couldn't connect \(address)!")
+            }
         }
     }
 }
--- btctl/BluetoothFramework.swift
+++ btctl/BluetoothFramework.swift
@@ -5,6 +5,7 @@
 //  Created by Martin Ivančo on 25/06/2022.
 //
 
+import Dispatch
 import IOBluetooth
 
 struct BluetoothFramework {
@@ -19,5 +20,23 @@ struct BluetoothFramework {
     static var connectedDevices: [BluetoothDevice]? {
         guard let paired = pairedDevices else { return nil }
         return paired.filter { $0.connected }
+    }
+    
+    static func connect(address: String, timeout: TimeInterval?) -> Bool {
+        let device = IOBluetoothDevice(addressString: address)
+        guard let t = timeout else {
+            return device?.openConnection() == kIOReturnSuccess
+        }
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        var result = false
+
+        DispatchQueue.main.async {
+            result = device?.openConnection() == kIOReturnSuccess
+            semaphore.signal()
+        }
+
+        _ = semaphore.wait(timeout: .now() + t)
+        return result
     }
 }
