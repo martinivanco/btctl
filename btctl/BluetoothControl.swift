700de1c63730fe07f854a6527865729b15c62ef8
Martin Ivanco <ivancom.fr@gmail.com>
2022-07-02 12:37:11 +0200

    Pairing devices

--- btctl/BluetoothControl.swift
+++ btctl/BluetoothControl.swift
@@ -13,7 +13,7 @@ struct BluetoothControl: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "btctl",
         abstract: "Bluetooth control utility",
-        subcommands: [Connected.self, Paired.self, Connect.self])
+        subcommands: [Connected.self, Paired.self, Connect.self, Pair.self])
     
     struct Connected: ParsableCommand {
         static let configuration = CommandConfiguration(abstract: "Get list of connected devices")
@@ -45,7 +45,7 @@ struct BluetoothControl: AsyncParsableCommand {
         @Argument(help: "The address of the device to connect")
         var address: String
         
-        @Option(name: .shortAndLong, help: "Optional timeout for the call in seconds")
+        @Option(name: .shortAndLong, help: "Optional timeout for the call in seconds (system will timeout the call after certain time automatically - this is just to make it shorter)")
         var timeout: TimeInterval?
 
         func run() async {
@@ -53,6 +53,24 @@ struct BluetoothControl: AsyncParsableCommand {
                 print("Successfully connected \(address).")
             } else {
                 print("Error: Couldn't connect \(address)!")
+            }
+        }
+    }
+    
+    struct Pair: AsyncParsableCommand {
+        static let configuration = CommandConfiguration(abstract: "Pair with a bluetooth device")
+
+        @Argument(help: "The address of the device to pair")
+        var address: String
+        
+        @Option(name: .shortAndLong, help: "Optional timeout for the call in seconds (default is 10 seconds)")
+        var timeout: TimeInterval?
+
+        func run() async {
+            if await BluetoothFramework.pair(address: address, timeout: timeout) {
+                print("Successfully paired \(address).")
+            } else {
+                print("Error: Couldn't pair \(address)!")
             }
         }
     }
--- btctl/BluetoothFramework.swift
+++ btctl/BluetoothFramework.swift
@@ -28,13 +28,7 @@ struct BluetoothFramework {
             return device?.openConnection() == kIOReturnSuccess
         }
         
-        let timeoutTask = Task { () -> Bool in
-            do {
-                try await Task.sleep(nanoseconds: UInt64(round(timeout * 1000000000)))
-            } catch { return true }
-            return false
-        }
-        
+        let timeoutTask = getTimeoutTask(timeout)
         let connectTask = Task { () -> Bool in
             let result = device?.openConnection() == kIOReturnSuccess
             timeoutTask.cancel()
@@ -43,5 +37,43 @@ struct BluetoothFramework {
         
         guard await timeoutTask.value else { return false }
         return await connectTask.value
+    }
+    
+    static func pair(address: String, timeout: TimeInterval?) async -> Bool {
+        let device = IOBluetoothDevice(addressString: address)
+        let pair = IOBluetoothDevicePair(device: device)
+        let delegate = PairDelegate()
+        pair?.delegate = delegate
+
+        guard let timeout = timeout else {
+            let finished: Bool = await withCheckedContinuation { continuation in
+                delegate.callback = { continuation.resume(returning: true) }
+                if pair?.start() != kIOReturnSuccess { continuation.resume(returning: false) }
+            }
+            
+            return finished && device?.isPaired() ?? false
+        }
+
+        let timeoutTask = getTimeoutTask(timeout)
+        delegate.callback = { timeoutTask.cancel() }
+        if pair?.start() != kIOReturnSuccess { return false }
+
+        guard await timeoutTask.value else { return false }
+        return device?.isPaired() ?? false
+    }
+    
+    @objcMembers
+    private class PairDelegate: NSObject, IOBluetoothDevicePairDelegate {
+        public var callback: (() -> Void)?
+        func devicePairingFinished(_ sender: Any!, error: IOReturn) { callback?() }
+    }
+    
+    private static func getTimeoutTask(_ timeout: TimeInterval) -> Task<Bool, Never> {
+        return Task { () -> Bool in
+            do {
+                try await Task.sleep(nanoseconds: UInt64(round(timeout * 1000000000)))
+            } catch { return true }
+            return false
+        }
     }
 }
